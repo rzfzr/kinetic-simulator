6ac373762549018a8a3cf6585714cdabaeb841e0
gustavogod <gustavodias.god@gmail.com>
2017-04-10 22:24:15 -0300

    Dado

---
+++ Dado/src/Unit2.pas
@@ -0,0 +1,57 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls,math;
+
+type
+  TForm2 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Button1: TButton;
+    Label7: TLabel;
+    Label8: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+  MeiaVidaCalculada:Extended;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+jogini,jogfin,dadoini,dadofin:Integer;
+begin
+
+jogini:=strtoint(edit1.Text);
+jogfin:=strtoint(edit2.Text);
+dadoini:=strtoint(edit3.Text);
+dadofin:=strtoint(edit4.Text);
+
+MeiaVidaCalculada:=((jogfin-jogini)*log10(2))/(log10(dadoini/dadofin));
+
+Label8.Caption:=floattostr(MeiaVidaCalculada);
+
+
+
+end;
+
+end.
