c4ea9a60cc8347d5c5a0df476914431367178d7d
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2017-06-03 23:00:24 -0300

    Reading csv file

---
+++ Simulator/src/simulator/csv.java
@@ -0,0 +1,33 @@
+package simulator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class csv {
+    
+    public static void main(String[] args) {
+        
+        File directory = new File("./");
+   System.out.println(directory.getAbsolutePath());
+            
+       File arquivoCSV = new File("../acervo.csv");
+        
+        try{
+            Scanner scanner = new Scanner(arquivoCSV);
+            String line = new String();            
+            scanner.nextLine();//ignores first line
+            
+            while(scanner.hasNext()){
+                line = scanner.nextLine();
+                String[] item = line.split(",");
+                System.out.println(item[1]);
+            }
+        
+        }catch(FileNotFoundException e){            
+                System.out.println("nope");
+        }
+        
+    }
+    
+}
